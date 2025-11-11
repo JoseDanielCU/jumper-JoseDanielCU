a4ff54574456cef55229dabe249d7f8e656fa1d3
Jose Daniel ¬_¬ <jcastaneda@xpetech.com>
2025-11-10 19:43:22 -0500

    Basic Jump

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerJump : MonoBehaviour
 {
@@ -10,28 +9,7 @@ public class PlayerJump : MonoBehaviour
     public Transform groundCheck;
     public LayerMask groundLayer;
     private bool isGrounded;
-    private bool wasGrounded = false;
     public float groundCheckRadius = 0.2f;
-
-    public InputActionReference jumpAction;
-
-    void OnEnable()
-    {
-        if (jumpAction != null && jumpAction.action != null)
-        {
-            jumpAction.action.performed += OnJumpPerformed;
-            jumpAction.action.Enable();
-        }
-    }
-
-    void OnDisable()
-    {
-        if (jumpAction != null && jumpAction.action != null)
-        {
-            jumpAction.action.performed -= OnJumpPerformed;
-            jumpAction.action.Disable();
-        }
-    }
 
     void Start()
     {
@@ -40,31 +18,21 @@ public class PlayerJump : MonoBehaviour
 
     void Update()
     {
-        // Check for ground (proteger por si groundCheck no está asignado)
-        bool currentlyGrounded = false;
-        if (groundCheck != null)
-        {
-            currentlyGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-        }
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-        // Log solo cuando pasa de no estar en suelo a estar en suelo
-        if (currentlyGrounded && !wasGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            Debug.Log($"Ground detected at {groundCheck?.position}");
-        }
-
-        isGrounded = currentlyGrounded;
-        wasGrounded = currentlyGrounded;
-    }
-
-    private void OnJumpPerformed(InputAction.CallbackContext ctx)
-    {
-        if (isGrounded)
-        {
-            // usar velocity correcto en Rigidbody2D
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
+
+        // --- Variable Jump Height ---
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
+        {
+            // If the button is released while jumping, cut the upward velocity
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
     }
+
 
     // Helper function to visualize the ground check radius in the Scene view
     private void OnDrawGizmosSelected()
